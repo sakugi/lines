7936554302c41d153f48f0b6d7b51e9a41033269
sakugi <128790650+sakugi@users.noreply.github.com>
2023-04-14 12:55:41 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,49 @@
-let x= 1;
-function setup() {
-  createCanvas(360, 640);
-  frameRate(60);
-}
+let x2 = 110;
+let y2 =30;
+let mic;
 
-function draw() {
+ function setup(){
+  //createCanvas((windowHeight/3), windowHeight);
+  mycanvas = createCanvas(120, 360);
+  background(255);
+  mic = new p5.AudioIn();
+  mic.start();
+  frameRate(15);
+  strokeCap(SQUARE);
+  loop();
+}
+ function blackLine(x1, y1, x2, y2){
+   stroke(0);
+   line (x1, y1, x2, y2);
+   
+ }
+ function whiteLine(x1, y1, x2, y2){
+   stroke(255);
+   line (x1, y1, x2, y2);
+   
+ }
+
+
+function draw(){      
   strokeWeight(6);
-  background(240);
-  line(x, 0, x, 1600);
-  x++;
-  if(x == 360){
-    x = 1;
+  micLevel = mic.getLevel();
+ // console.log(micLevel);
+  x2 --;
+  whiteLine(11, y2, x2, y2)
+  //if(y2 < (windowHeight-30)){
+  if(y2 < 330){
+      if (micLevel > 0.05){
+        blackLine(11, y2, (x2), y2)
+      }
+    }
+  if (x2 == 10){
+      x2 = 110;
+      y2 = y2 +10;
+      }
+  if(y2==330){
+     saveCanvas(mycanvas,"present moment","png");
+     clear();    
+     background(255);
+     y2 = 30;
   }
 }
