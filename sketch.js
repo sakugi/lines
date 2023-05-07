6a56f998dc803dbf33c97b3d4fa9185ab9c7d5aa
sakugi <128790650+sakugi@users.noreply.github.com>
2023-05-07 21:32:51 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,6 +23,10 @@ function touchStarted(){
  waitForClick = false;
  return false;
 }
+function touchMoved() { //new check
+mouseClicked();
+return false;
+}
  function blackLine(x1, y1, x2, y2){
    stroke(0);
    line (x1, y1, x2, y2);
