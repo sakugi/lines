a4ba100d2fc1cedb2af35ede6e9ae217641b07cf
sakugi <128790650+sakugi@users.noreply.github.com>
2023-05-08 00:36:08 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,28 +1,29 @@
 let x2 = 110;
 let y2 =30;
 let mic;
-let waitForClick = true;
+// let waitForClick = true;
 
  function setup(){
   //getAudioContext().suspend();
-  mycanvas = createCanvas(120, 360);
+ let cnv = createCanvas(120, 360);
   background(255);
-  //mic = new p5.AudioIn();
-  //mic.start();
+  cnv.mousePressed(userStartAudio);
+  mic = new p5.AudioIn();
+  mic.start();
   frameRate(15);
   strokeCap(SQUARE);
   strokeWeight(6);
   loop();
 }
-function touchStarted(){
- if (!waitForClick) return;
- userStartAudio();
- mic = new p5.AudioIn();
- background(255);
- mic.start();
- waitForClick = false;
- return false;
-}
+//function touchStarted(){
+ //if (!waitForClick) return;
+ //userStartAudio();
+ //mic = new p5.AudioIn();
+ //background(255);
+ //mic.start();
+ //waitForClick = false;
+ //return false;
+//}
  function blackLine(x1, y1, x2, y2){
    stroke(0);
    line (x1, y1, x2, y2);
@@ -36,10 +37,10 @@ function touchStarted(){
 
 
 function draw(){  
-  if (waitForClick) {
-    background(33);
-   return;
- } 
+ // if (waitForClick) {
+   // background(33);
+   //return;
+// } 
   //strokeWeight(60);
   micLevel = mic.getLevel();
  // console.log(micLevel);
@@ -55,7 +56,7 @@ function draw(){
       y2 = y2 +10;
       }
   if(y2==330){
-     saveCanvas(mycanvas,"present moment_" + Date.now(),"png");
+     saveCanvas(cnv,"present moment_" + Date.now(),"png");
      clear();    
      background(255);
      y2 = 30;
