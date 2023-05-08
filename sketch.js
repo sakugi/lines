08007b8a12acc46071111024cd4daa9a41dccfa1
sakugi <128790650+sakugi@users.noreply.github.com>
2023-05-08 18:50:06 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -47,7 +47,7 @@ function draw(){
   x2 --;
   whiteLine(11, y2, x2, y2)
   if(y2 < 330){
-      if (micLevel > 0.02){
+      if (micLevel > 0.015){
         blackLine(11, y2, (x2), y2)
       }
     }
